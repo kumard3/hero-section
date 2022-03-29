ac97366b80a3370ea9a8ac33a157b2d9bbdbfffb
kumard3 <kumardeepanshu157200@gmail.com>
2022-03-30 00:56:48 +0530

    new component

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,19 @@ import HeroSection from "../components/HeroSection";
 export default function Home() {
   return (
     <div className="min-h-screen  text-center dark:bg-black dark:text-gray-200">
-      <HeroSection />
+      <div className=" container mx-auto">
+        <div className="flex ">
+          <section>
+            <div>
+              NFT that are 
+            </div>
+            <img src="KEEPSMILING.png" />
+          </section>
+          <div>
+            <img src="nft.png" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
