deee53b150c18a0a9137534fedb4ec1990a0331d
kumard3 <kumardeepanshu157200@gmail.com>
2022-03-31 09:04:32 +0530

    basic layout added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,50 @@
 /* eslint-disable @next/next/link-passhref */
-
+import Image from "next/image";
 import HeroSection from "../components/HeroSection";
 
 export default function Home() {
   return (
     <div className="min-h-screen  text-center dark:bg-black dark:text-gray-200">
-      <div className=" container mx-auto">
-        <div className="flex ">
-          <section>
-            <div>
-              NFT that are 
+      <div className="relative min-h-screen">
+        <div className="absolute inset-0 -z-0 opacity-95">
+          {" "}
+          <Image src="/background.png" layout="fill" />{" "}
+        </div>
+
+        <div className=" relative z-10 flex h-full w-full justify-between px-10  py-10 ">
+          <section className="flex   flex-col items-center justify-center">
+            <div className="relative w-full max-w-xl">
+              <h1 className="flex flex-col text-left font-mono text-7xl font-bold">
+                NFT that are Out of this{" "}
+                <span className="flex items-center">
+                  world
+                  <div className="">
+                    <Image src="/KEEPSMILING.png" width="60" height="60" />
+                  </div>{" "}
+                </span>{" "}
+              </h1>
             </div>
-            <img src="KEEPSMILING.png" />
+            <div className="max-w-xl">
+              <p className="py-12 text-left">
+                {" "}
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Reprehenderit nostrum, ab perspiciatis, illo laudantium libero
+                quo praesentium eveniet quae saepe deserunt aliquid ducimus
+                quibusdam. Veniam earum perspiciatis laudantium rem cupiditate.{" "}
+              </p>
+            </div>
+            <div className="relative flex w-full justify-around">
+              <button className="relative flex h-10 w-32 items-center justify-center ">
+                <div className=" absolute  h-16 w-48 skew-x-[23deg] items-center justify-center  bg-gradient-to-r from-[#B1D26F]  to-[#60E7CE]	"></div>
+                <h1 className="absolute z-10 font-mono text-2xl font-bold text-black ">
+                  Explore
+                </h1>
+              </button>
+              <button className="font-mono text-2xl font-bold ">Learn more</button>
+            </div>
           </section>
-          <div>
-            <img src="nft.png" />
+          <div className="hidden  md:block ">
+            <Image src="/nft.png" width="600" height="600" />
           </div>
         </div>
       </div>
